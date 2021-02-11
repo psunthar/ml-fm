22b82bbd686aa81bab80c131c08dd04c9dcd816a
Ram Kaistha <ramkaistha7@outlook.com>
2021-02-11 12:52:50 +0530

    fixed shape of image, variable dimesnions related to pipe

--- img.py
+++ img.py
@@ -3,12 +3,13 @@ import matplotlib.cm as cm
 import numpy as np
 from numpy import save
 imageH=4
-for r in range(1,100):
-    l=np.random.uniform(2,20)
-    Tl=np.random.uniform(50,50)
-    l1=2
+for r in range(1,10):
+    l1=np.random.uniform(1,5)   #upto stenosis
+    l=np.random.uniform(2,20)   #stenosis
+    Tl=np.random.uniform(40,50) #total length
+    
     AR=np.random.uniform(1.1,7)
-    d0=2
+    d0=np.random.uniform(0.5,4)
     d1=(d0)/np.sqrt(AR)
     k=imageH-d0
     k1=imageH-d1
@@ -16,29 +17,31 @@ for r in range(1,100):
 
 
     def f1(x):
-        if x<2:
+        if x<l1:
             return k
-        if x>=2 and x<2+l:
+        if x>=l1 and x<l1+l:
             return k1
-        if x>=2+l:
+        if x>=l1+l:
             return k
         
 
 
-    xmax=Tl
+    
     dx=0.1
+    #img=np.ones((600,100))
     img=np.zeros((int(Tl/dx),int(imageH/dx)))
 
-    for i in range(int(xmax/dx)):
+    for i in range(int(Tl/dx)):
         x=i*dx
         for j in range(int(f1(x)/dx)):
        # print([i,j,f1(x)])
             img[i,j]=1
-
-
+    
+    b=np.ones((int(700-Tl/dx),40))  
+    img=np.append(img,b,axis=0)        #adding rows of zeros at the end of matrix to give uniform shape
+    #img[:-1,:]=a
+    print(np.shape(img))
     img1=img.T
-
-
     plt.imsave('filename.png', np.array(img1), cmap=cm.gray)
     img2=np.flip(img1,0)
     img3=np.concatenate((img1, img2))
@@ -48,6 +51,6 @@ for r in range(1,100):
         
         a=np.dstack((a,img3))
     
-        print(np.shape((a)))
+       # print(np.shape((a)))
     plt.imsave('image' +str(r)+'.png', np.array(img3), cmap=cm.gray)
 save('data', a)
