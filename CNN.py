b07eefc1cd64401e6beaf7c1992d7c7ffb50d562
Ram Kaistha <ramkaistha7@outlook.com>
2021-05-17 13:49:17 +0530

    deleted couple of maxpooling layer and decreased batch size to 16 (feature space increased) reduced epochs to 90 minor change in number and dimension of filters

--- CNN.py
+++ CNN.py
@@ -1,8 +1,9 @@
+
 import os
 #import tensorflow as tf
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = "2"
 
-import numpy as np
+import numpy as np	
 import pandas as pd
 import sklearn
 from sklearn.model_selection import train_test_split
@@ -17,7 +18,7 @@ from keras.layers import Flatten
 import matplotlib.pyplot as plt
 from numpy import save
 from keras.layers import BatchNormalization
-
+from keras import regularizers
 ############################################
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
@@ -36,44 +37,55 @@ session = InteractiveSession(config=config)
 
 
 Xdata = load('rotate_data.npy')
-df=pd.read_csv("4_outputs.csv")
+df=pd.read_csv("3_outputs.csv")
 df=df.values
 
 Y=np.dstack(df)
 
-X_train, X_test, y_train, y_test =train_test_split(Xdata.T,Y.T,test_size=0.3)
+X_train, X_test, y_train, y_test =train_test_split(Xdata.T,Y.T,test_size=0.1)
 print(np.shape(X_train))
-X_train = X_train.reshape(-1, 1798,1799, 1)
-X_test = X_test.reshape(-1, 1798, 1799, 1)
+X_train = X_train.reshape(-1, 600, 600, 1)
+X_test = X_test.reshape(-1, 600, 600, 1)
 model =Sequential()
-model.add(Conv2D(4,(9,9),activation= 'relu'))
+model.add(Conv2D(4,(5,5),activation= 'relu'))
+#model.add(Dropout(0.5))
 #model.add(BatchNormalization())
+#model.add(MaxPooling2D((2,2)))
+model.add(Conv2D(8,(5,5),activation= 'relu'))
+#model.add(BatchNormalization())
+
+#model.add(Dropout(0.2))
+#model.add(MaxPooling2D((2,2)))
+model.add(Conv2D(16,(3,3), activation= 'relu'))
 model.add(MaxPooling2D((2,2)))
-model.add(Conv2D(8,(7,7),activation= 'relu'))
-model.add(MaxPooling2D((2,2)))
-model.add(Conv2D(16,(5,5), activation= 'relu'))
-model.add(MaxPooling2D((2,2)))
-model.add(Conv2D(16,(5,5), activation= 'relu'))
-model.add(MaxPooling2D((2,2)))
+#model.add(BatchNormalization())
+
 model.add(Conv2D(32,(3,3), activation= 'relu'))
 model.add(MaxPooling2D((2,2)))
-model.add(Conv2D(64,(1,1), activation= 'relu'))
+#model.add(BatchNormalization())
+
+model.add(Conv2D(64,(3,3), activation= 'relu'))
+model.add(MaxPooling2D((2,2)))
+#model.add(BatchNormalization())
+
+model.add(Conv2D(128,(1,1), activation= 'relu'))
 model.add(MaxPooling2D((2,2)))
 model.add(Flatten())
-model.add(Dense(60, activation='relu'))
+model.add(Dense(50, activation='relu'))
+#model.add(Dropout(0.2))
 model.add(Dense(30, activation='relu'))
 model.add(Dense(20,activation='relu'))
+#model.add(Dropout(0.1))
 model.add(Dense(10,activation='relu'))
-model.add(Dense(4, activation='linear'))
+model.add(Dense(3, activation='linear'))
 opt = keras.optimizers.Adam(learning_rate=0.001)
 model.compile(optimizer=opt,loss='mse')
-model.fit(x=X_train,y=y_train,batch_size=64,validation_data=(X_test,y_test),epochs=150)
-sol=model.predict(X_test)
+model.fit(x=X_train,y=y_train,batch_size=16,validation_data=(X_test,y_test),epochs=90)
+sol=model.predict(X_test) 
 print(sol)
 save('prediction',sol)        # saving predicted results
 save('testData',y_test)
-#plt.scatter(y_test.reshape(30),sol.reshape(30))
-#plt.xlabel('original')
-#plt.ylabel('predicted')
-#plt.show()
+
+
+
 
